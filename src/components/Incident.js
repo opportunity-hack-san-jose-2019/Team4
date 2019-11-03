cd2eef7df64fb6470d9c177e23fdde5671db5e31
Gustavo Zapata <gustavozapataq@hotmail.com>
2019-11-02 20:35:07 -0700

    added styling to the page

--- src/components/Incident.js
+++ src/components/Incident.js
@@ -3,10 +3,39 @@ import React from "react";
 export default function Incident(props) {
   return (
     <div className="Incident">
-      <h1>{props.name}</h1>
-      <h3>{props.location}</h3>
-      <img src={require(`../images/${props.image}.jpg`)} />
-      <p>{props.priority}</p>
+      <img
+        className="priority-img"
+        src={require(`../images/${props.incident.priority}.png`)}
+      />
+      <h1>{props.incident.name}</h1>
+      <br />
+      <img className="location-img" src={require("../images/location.png")} />
+      <h3>{props.incident.location}</h3>
+      <img
+        className="incident-img"
+        src={require(`../images/${props.incident.image}.jpg`)}
+      />
+      <button className="category">{props.incident.category}</button>
+      <div className="posted-by">
+        <img className="creator-img" src={require("../images/charity.png")} />
+        <p>
+          Posted by <br />
+          <span>Charity Name</span>
+        </p>
+      </div>
+      <p className="description">{props.incident.description}</p>
+      <div className="actions">
+        <div className="action-donate">
+          <img src={require("../images/donate.png")} />
+          <p>{props.incident.donation}</p>
+          <button>Donate</button>
+        </div>
+        <div>
+          <img src={require("../images/volunteer.png")} />
+          <p>{props.incident.volunteers}</p>
+          <button>Volunteer</button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Incidents.js
+++ src/components/Incidents.js
@@ -9,22 +9,37 @@ export class Incidents extends Component {
         id: 1,
         name: "Wildfire destroys over 100 houses",
         location: "San Jose, CA",
+        description:
+          "Mr Chesky said Airbnb would create a dedicated 'party house' rapid response team and expand manual screening of high-risk reservations. The company, which is expected to float on the stock market in 2020, would also take action against users who violated its policies, he said.",
+        category: "Natural Disaster",
         image: "wildfire",
-        priority: "High"
+        priority: "high",
+        donation: "$1,355",
+        volunteers: "105"
       },
       {
         id: 2,
         name: "Flooding caused by gargabe",
         location: "San Francisco, CA",
+        description:
+          "Mr Chesky said Airbnb would create a dedicated 'party house' rapid response team and expand manual screening of high-risk reservations. The company, which is expected to float on the stock market in 2020, would also take action against users who violated its policies, he said.",
+        category: "Natural Disaster",
         image: "flooding",
-        priority: "Medium"
+        priority: "medium",
+        donation: "$1,610",
+        volunteers: "187"
       },
       {
         id: 3,
         name: "Homeless people needing clothes",
         location: "Sunnyvale, CA",
+        description:
+          "Mr Chesky said Airbnb would create a dedicated 'party house' rapid response team and expand manual screening of high-risk reservations. The company, which is expected to float on the stock market in 2020, would also take action against users who violated its policies, he said.",
+        category: "Humanity",
         image: "homeless",
-        priority: "Low"
+        priority: "low",
+        donation: "$823",
+        volunteers: "65"
       }
     ]
   };
@@ -34,12 +49,7 @@ export class Incidents extends Component {
       <div className="Incidents">
         <h1>Incidents</h1>
         {this.state.incidents.map(el => (
-          <Incident
-            name={el.name}
-            location={el.location}
-            image={el.image}
-            priority={el.priority}
-          />
+          <Incident incident={el} />
         ))}
       </div>
     );
